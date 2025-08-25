ff634252ab6947672480333649d88e358269cf7e
Tilent Atsiaya <tilentatsiaya@gmail.com>
2025-08-25 19:27:57 +0300

    initial commit to fundamentals of SQL

---
+++ WK1/answers.sql
@@ -0,0 +1,6 @@
+-- Write an SQL query to create a new database called salesDB
+CREATE DATABASE IF NOT EXISTS salesDB;
+
+
+-- Write an SQL query to drop (delete) the database called demo.
+DROP DATABASE IF EXISTS demo
