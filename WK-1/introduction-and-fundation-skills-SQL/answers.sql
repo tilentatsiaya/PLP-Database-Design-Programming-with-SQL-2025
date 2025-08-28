eeb57954c48c82d5011f275fdb099d255688d31a
Tilent Atsiaya <tilentatsiaya@gmail.com>
2025-08-28 17:07:38 +0300

    Update answers.sql
    updated querry

--- WK-1/introduction-and-fundation-skills-SQL/answers.sql
+++ WK-1/introduction-and-fundation-skills-SQL/answers.sql
@@ -1,6 +1,6 @@
 -- Write an SQL query to create a new database called salesDB!
-CREATE DATABASE IF NOT EXISTS salesDB;
+CREATE DATABASE salesDB;
 
 
 -- Write an SQL query to drop (delete) the database called demo!
-DROP DATABASE IF EXISTS demo
+DROP DATABASE demo
