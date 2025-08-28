48553c46bf6fea27a95b78af77caea3b1c2ac5f2
Tilent Atsiaya <tilentatsiaya@gmail.com>
2025-08-28 17:31:34 +0300

    This is updated commits to SQL

--- WK-3/answers.sql
+++ WK-3/answers.sql
@@ -1,21 +1,22 @@
-USE salesDB;
 
 -- Question 1: Create a table called 'student'
 CREATE TABLE student(
-id INT PRIMARY KEY,
-Fullname TEXT(100) NOT NULL,
+student_id INT PRIMARY KEY,
+Fullname VARCHAR(100) NOT NULL,
 age INT
 );
 
 -- Question 2: Insert at least 3 records into the 'student' table.
-INSERT INTO student(id,Fullname,age)
-VALUES
-(38903445, 'Tilent Azush', 34),
-(5679055, 'Samuel Dick', 24),
-(37894255,'Bravin Tom',30);
+INSERT INTO student(id,Fullname,age),
+VALUES(1, 'Tilent Azush', 34),
+(2, 'Samuel Dick', 24),
+(3,'Bravin Tom',30),
+(4,'Brain Mbara',30),
+(5,'Hillary Jack',34),
+(6,'Abel Juniour',43);
 
 -- Question 3: Update the age of the student with ID 2 to 20
 UPDATE student
 SET age = 20
-WHERE id = 2;
+WHERE student_id = 2;
 
